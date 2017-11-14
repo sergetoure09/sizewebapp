cefcb4d82e93d7abc2febf4a856d81a73ac42c95
sergetoure09 <sergetoure09@gmail.com>
2017-11-14 23:49:44 +0000

    adding flask web pages

---
+++ webserv.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+@app.route('/')
+def form():
+    return render_template("form.html")
+
+@app.route('/success/')
+def success():
+    return render_template("success.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
+
